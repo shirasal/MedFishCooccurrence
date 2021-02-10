2a0c91046bf5fd24883fb7977c1669937a040646
Shira Salingre <shira.salin@gmail.com>
2021-02-10 19:49:26 +0200

    created species matrices

--- R/data_preps.R
+++ R/data_preps.R
@@ -1,1 +1,90 @@
 source("R/packages.R")
+
+
+# Data wrangling for MEData -----------------------------------------------
+
+medata <- read_rds("data/medata.Rds")
+str(medata)
+
+# Create a dataset with relevant information, scaled covariates, and boolean MPA column:
+med_clean <- medata %>%
+  filter(data.origin != "azz_asi") %>% # presence-absence
+  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
+         temp = as.vector(scale(tmean)),
+         depth = as.vector(scale(depth)),
+         sal = as.vector(scale(sal_mean)),
+         prod = as.vector(scale(pp_mean))) %>%
+  select(site, lon, lat, trans, species, sp.n, mpa, temp, depth, prod)
+
+# Herbivores require another filtering:
+medata %>% distinct(country)
+med_clean_east <- medata %>%
+  filter(country == "Israel" | country == "Greece" | country == "Turkey") %>% 
+  filter(data.origin != "azz_asi") %>% # presence-absence
+  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
+         temp = as.vector(scale(tmean)),
+         depth = as.vector(scale(depth)),
+         sal = as.vector(scale(sal_mean)),
+         prod = as.vector(scale(pp_mean))) %>%
+    select(site, lon, lat, trans, species, sp.n, mpa, temp, depth, prod)
+
+# Create assisting vectors ------------------------------------------------
+
+# Guilds
+groupers <- c("Epinephelus.costae", "Epinephelus.marginatus", "Serranus.cabrilla", "Serranus.scriba")
+diplodus <- c("Diplodus.annularis", "Diplodus.puntazzo", "Diplodus.sargus", "Diplodus.vulgaris")
+herbivores <- c("Siganus.rivulatus", "Siganus.luridus", "Sarpa.salpa", "Sparisoma.cretense")
+
+# Covariates (based on med_clean column names)
+env_cov <- c("temp", "depth", "prod")
+mpa_cov <- "mpa"
+
+
+# Create species matrix (for MRFcov) --------------------------------------
+
+# Define function
+create_spp_mat <- function(dataset, guild, covariate){
+  cols <- c(c("lat", "lon", "site", "trans", "species"), env_cov, mpa_cov)
+  dataset %>%
+    group_by_at(.vars = cols) %>%
+    summarise(n = sum(sp.n), .groups = "drop") %>% 
+    spread(species, n, fill = 0) %>% 
+    # Add unique rownames that describe the site and transect:
+    mutate(loc = paste(site, trans)) %>% 
+    group_by(loc) %>%
+    column_to_rownames("loc") %>% 
+    select(all_of(guild), all_of(covariate)) %>% 
+    ungroup()
+}
+
+# Combine covariate vectors:
+all_covs <- c(env_cov, mpa_cov)
+
+# Create matrices for each guild:
+grps_mat <- create_spp_mat(dataset = med_clean, guild = groupers, covariate = all_covs)
+dip_mat <- create_spp_mat(dataset = med_clean, guild = diplodus, covariate = all_covs)
+herb_mat <- create_spp_mat(dataset = med_clean_east, guild = herbivores, covariate = all_covs)
+
+# Remove NAs, but keep track of what was removed:
+grps_NAs <- grps_mat %>% as_tibble(rownames = NA) %>% rownames_to_column("ID") %>% 
+  filter(is.na(temp) | is.na(depth) | is.na(prod) | is.na(mpa)) %>% 
+  select(ID, all_of(all_covs))
+nrow(grps_NAs) # 529 observations removed from analysis due to missing information
+grps_mat %<>% filter(!is.na(temp), !is.na(depth), !is.na(prod), !is.na(mpa))
+
+dip_NAs <- dip_mat %>% as_tibble(rownames = NA) %>% rownames_to_column("ID") %>% 
+  filter(is.na(temp) | is.na(depth) | is.na(prod) | is.na(mpa)) %>% 
+  select(ID, all_of(all_covs))
+nrow(dip_NAs) # 529 observations removed from analysis due to missing information
+dip_mat %<>% filter(!is.na(temp), !is.na(depth), !is.na(prod), !is.na(mpa))
+
+herb_NAs <- herb_mat %>% as_tibble(rownames = NA) %>% rownames_to_column("ID") %>% 
+  filter(is.na(temp) | is.na(depth) | is.na(prod) | is.na(mpa)) %>% 
+  select(ID, all_of(all_covs))
+nrow(dip_NAs) # 529 observations removed from analysis due to missing information
+herb_mat %<>% filter(!is.na(temp), !is.na(depth), !is.na(prod), !is.na(mpa))
+
+setdiff(grps_NAs, dip_NAs, herb_NAs)
+
+locations_removed <- grps_NAs %>% distinct()
+rm(grps_NAs, dip_NAs, herb_NAs)
