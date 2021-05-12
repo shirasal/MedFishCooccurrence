ab19fede6189dfd8b11d148722d6238e851b1641
Shira Salingre <shira.salin@gmail.com>
2021-05-12 16:06:45 +0300

    ran biomass models

--- R/models.R
+++ R/models.R
@@ -77,7 +77,7 @@ names(species_mats_mass) <- c("grps_mass_mat", "dip_mass_mat", "herb_mass_mat")
 biomass_models <- lapply(species_mats_mass, function(x){MRFcov(x, n_nodes = 4, family = "gaussian")})
 names(biomass_models) <- c("grps_mass", "dip_mass", "herb_mass")
 
-biomass_models$grps_mass$direct_coefs
+biomass_models$dip_mass$key_coefs
 
 ## Relative importance ----------------------------------------------------
 
@@ -87,5 +87,50 @@ names(mass_relimp) <- c("grps_mass_relimp", "dip_mass_relimp", "herb_mass_relimp
 mass_relimp$grps_mass_relimp
 mass_relimp$dip_mass_relimp
 mass_relimp$herb_mass_relimp
+
+cov_titles <- tibble(covariate = c("env", "mpa", "bio", "temp_bio", "mpa_bio"),
+                     facet.title = factor(c("Environment", "MPA", "Biotic Associations",
+                                            "Temp * Biotic", "MPA * Biotic"),
+                                          levels = c("Environment", "MPA", "Biotic Associations",
+                                                     "Temp * Biotic", "MPA * Biotic")))
+
+p_relimp_grps_mass <- mass_relimp$grps_mass_relimp %>% plot_relimp("grps", "Groupers")
+p_relimp_dip_mass <- mass_relimp$dip_mass_relimp %>% select(-`NA`) %>% 
+  pivot_longer(2:length(.)) %>%
+  rename(covariate = name, rel_imp = value) %>%
+  mutate(species = str_replace_all(species, "\\.", "\\ ")) %>%
+  right_join(cov_titles, by = "covariate") %>% 
+  add_row(species = diplodus, covariate = "temp_bio", rel_imp = NA, facet.title = factor("Temp * Biotic",
+          levels = c("Environment", "MPA", "Biotic Associations",
+                     "Temp * Biotic", "MPA * Biotic"))) %>%  
+  filter(!is.na(species)) %>% 
+  # Plot:
+  ggplot() +
+  aes(x = species, y = rel_imp) +
+  stat_summary(geom = "bar", fun = mean, position = "dodge",  fill = guild_colours$dip) +
+  facet_wrap(~facet.title, nrow = 1) +
+  labs(subtitle = "Seabreams", y = "Relative Importance (prop.)") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "italic"), strip.placement = "outside",
+        axis.title.x = element_blank(), 
+        strip.text.x = element_text(size = 12, face = "bold"),
+        plot.margin = margin(.2,1,.2,1, "cm"))
+p_relimp_herb_mass <- mass_relimp$herb_mass_relimp %>% select(-`NA`) %>% plot_relimp("herb", "Herbivores")
+
+# ggsave(plot = p_relimp_grps_mass, filename = "figures/relimp_grps_mass.png", device = "png",
+#        dpi = 300, width = 11.74, height = 4, units = "in")
+# ggsave(plot = p_relimp_dip_mass, filename = "figures/relimp_dip_mass.png", device = "png",
+#        dpi = 300, width = 11.74, height = 4, units = "in")
+# ggsave(plot = p_relimp_herb_mass, filename = "figures/relimp_herb_mass.png", device = "png",
+#        dpi = 300, width = 11.74, height = 4, units = "in")
+
+patch_plot_mass <- p_relimp_grps_mass / p_relimp_dip_mass / p_relimp_herb_mass
+# Remove y axis titles from first and third subplots and enlarge the middle one's
+patch_plot_mass[[1]] <- patch_plot_mass[[1]] + theme(axis.title.y = element_blank())
+patch_plot_mass[[2]] <- patch_plot_mass[[2]] + theme(axis.title.y = element_text(size = 14, vjust = 5))
+patch_plot_mass[[3]] <- patch_plot_mass[[3]] + theme(axis.title.y = element_blank())
+
+patch_plot_mass
+# ggsave("figures/rel_imp_mass.png", device = "png",
+#        dpi = 150, height = 10, width = 10, units = "in")
 
 
