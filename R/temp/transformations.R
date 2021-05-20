57dee55cf4ad3c4e5f207bc05d9d5d7d6ba6fb40
Shira Salingre <shira.salin@gmail.com>
2021-05-20 10:26:13 +0300

    added nonparanormal transformation visualisations

--- R/temp/transformations.R
+++ R/temp/transformations.R
@@ -49,7 +49,6 @@ med_clean %>% filter(species %in% relevant_species$groupers) %>%
   ggeasy::easy_rotate_x_labels() + 
   ggtitle("SQRT")
 
-
 # Seabreams
 
 ## No transformation
@@ -117,4 +116,55 @@ med_clean %>% filter(species %in% relevant_species$herbivores) %>%
   ggeasy::easy_rotate_x_labels() + 
   ggtitle("SQRT")
 
+
+# Nonparanormal -----------------------------------------------------------
+
+med_clean %>% filter(species %in% unlist(relevant_species)) %>% 
+  mutate(npn = qnorm(rank(log2(sp.n + 0.01)) / (length(sp.n) + 1))) %>% 
+  mutate(guild = case_when(species %in% unlist(relevant_species[1]) ~ names(relevant_species[1]),
+                           species %in% unlist(relevant_species[2]) ~ names(relevant_species[2]),
+                           species %in% unlist(relevant_species[3]) ~ names(relevant_species[3]))) %>% 
+  distinct(site, species, guild, npn) %>% 
+  ggplot() + 
+  aes(species, npn, col = site) + 
+  geom_point(alpha = .2) + 
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90, hjust = 1)) +
+  ggtitle("Nonparanormal") +
+  facet_wrap(~guild, scales = "free")
+
+
+med_clean %>% filter(species %in% unlist(relevant_species)) %>% 
+  mutate(npn = qnorm(rank(log2(sp.n + 0.01)) / (length(sp.n) + 1))) %>% 
+  mutate(guild = case_when(species %in% unlist(relevant_species[1]) ~ names(relevant_species[1]),
+                           species %in% unlist(relevant_species[2]) ~ names(relevant_species[2]),
+                           species %in% unlist(relevant_species[3]) ~ names(relevant_species[3]))) %>% 
+  mutate(guild = factor(guild, levels = names(relevant_species))) %>%
+  distinct(site, species, guild, npn) %>% 
+  ggplot() + 
+  aes(species, npn) + 
+  geom_boxplot(aes(col = guild)) + 
+  geom_hline(aes(yintercept = 0), col = "black") + 
+  scale_color_manual(values = as.vector(unlist(guild_colours))) +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90, hjust = 1)) +
+  ggtitle("Nonparanormal") +
+  facet_wrap(~guild, scales = "free")
+
+med_clean %>% filter(species %in% unlist(relevant_species)) %>% 
+  mutate(npn = log2(sp.n + 0.01)) %>% 
+  mutate(guild = case_when(species %in% unlist(relevant_species[1]) ~ names(relevant_species[1]),
+                           species %in% unlist(relevant_species[2]) ~ names(relevant_species[2]),
+                           species %in% unlist(relevant_species[3]) ~ names(relevant_species[3]))) %>% 
+  mutate(guild = factor(guild, levels = names(relevant_species))) %>%
+  distinct(site, species, guild, npn) %>% 
+  ggplot() + 
+  aes(species, npn) + 
+  geom_boxplot(aes(col = guild)) + 
+  geom_hline(aes(yintercept = 0), col = "black") + 
+  scale_color_manual(values = as.vector(unlist(guild_colours))) +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90, hjust = 1)) +
+  ggtitle("Nonparanormal (old transform)") +
+  facet_wrap(~guild, scales = "free")
 
