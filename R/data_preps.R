2904b741fc473885dfe5dac105840c8dc30ef5ba
Shira Salingre <shira.salin@gmail.com>
2021-06-27 11:23:55 +0300

    datasets  now with SCALED biomass

--- R/data_preps.R
+++ R/data_preps.R
@@ -14,7 +14,7 @@ med_clean <- medata %>%
          depth = as.vector(scale(depth)),
          sal = as.vector(scale(sal_mean)),
          prod = as.vector(scale(pp_mean)),
-         biomass = a*sp.length^b) %>%
+         biomass = as.vector(scale(a*sp.length^b))) %>%
   select(site, lon, lat, trans, species, sp.n, biomass, mpa, temp, depth, prod)
 
 # Herbivores require another filtering:
@@ -27,7 +27,7 @@ med_clean_east <- medata %>%
          depth = as.vector(scale(depth)),
          sal = as.vector(scale(sal_mean)),
          prod = as.vector(scale(pp_mean)),
-         biomass = a*sp.length^b) %>%
+         biomass = as.vector(scale(a*sp.length^b))) %>%
     select(site, lon, lat, trans, species, sp.n, biomass, mpa, temp, depth, prod)
 
 write_rds(med_clean, "data/processed/med_clean.rds")
@@ -161,3 +161,4 @@ write_rds(herb_mass_mat, "data/processed/herb_mass_mat.rds")
 
 guild_colours <- list(grps = "#c54607", dip = "#145d82", herb = "#43aa8b")
 write_rds(guild_colours, "data/processed/guild_colours.rds")
+
