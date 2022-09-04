6ae57fc7939925f4d40e0f0a8eb0bbe04c51143f
Shira Salingré <shira.salin@gmail.com>
2022-09-04 23:09:10 +0200

    removed redundant 'cols' assignment

--- R/create_spp_mat_function.R
+++ R/create_spp_mat_function.R
@@ -4,19 +4,18 @@
 # This function turns an abundance/biomass dataset (long format) into a matrix
 # It also scales the numeric covariates for abundance matrix, and all values in biomass matrix
 create_spp_mat <- function(dataset, guild, metric, covariate){
-  cols <- c(c("lat", "lon", "site", "trans", "species"), env_cov, mpa_cov)
   if (metric == "sp.n" | metric == "abundance") {
     dataset %>% 
       filter(species %in% guild,
              !is.na(depth)) %>% 
-      pivot_wider(names_from = species, values_from = sp.n, values_fill = 0, values_fn = sum) %>% 
+      pivot_wider(names_from = species, values_from = sp.n, values_fn = function(x){sum(x, na.rm = TRUE)}, values_fill = 0) %>% 
       select(all_of(guild), all_of(covariate)) %>% 
       mutate(across(.cols = c(all_of(guild),all_of(env_cov)), .fns = function(x){as.vector(scale(x))}))
   } else if(metric == "biomass") {
     dataset %>%
       filter(species %in% guild,
              !is.na(depth)) %>% 
-      pivot_wider(names_from = species, values_from = biomass, values_fill = 0, values_fn = sum) %>% 
+      pivot_wider(names_from = species, values_from = biomass, values_fn = function(x){sum(x, na.rm = TRUE)}, values_fill = 0) %>% 
       select(all_of(guild), all_of(covariate)) %>% 
       mutate(across(.cols = c(all_of(guild),all_of(env_cov)), .fns = function(x){as.vector(scale(x))}))
   } else {
