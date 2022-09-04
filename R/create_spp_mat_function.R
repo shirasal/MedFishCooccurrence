15e67740203950de7b9179baf204eca22c659aae
Shira Salingré <shira.salin@gmail.com>
2022-09-04 22:40:52 +0200

    more coherent and simple species matrices creation function, without rownames by sites because it doesn't matter

--- R/create_spp_mat_function.R
+++ R/create_spp_mat_function.R
@@ -7,30 +7,18 @@ create_spp_mat <- function(dataset, guild, metric, covariate){
   cols <- c(c("lat", "lon", "site", "trans", "species"), env_cov, mpa_cov)
   if (metric == "sp.n" | metric == "abundance") {
     dataset %>% 
-      group_by_at(.vars = cols) %>%
-      summarise(n = sum(sp.n), .groups = "drop") %>% 
-      spread(species, n, fill = 0) %>% 
-      # Add unique rownames that describe the site and transect:
-      mutate(loc = make.unique(paste(site, trans, sep = "_"))) %>% 
-      group_by(loc) %>%
-      column_to_rownames("loc") %>% 
-      filter(!is.na(depth)) %>% 
+      filter(species %in% guild,
+             !is.na(depth)) %>% 
+      pivot_wider(names_from = species, values_from = sp.n, values_fill = 0, values_fn = sum) %>% 
       select(all_of(guild), all_of(covariate)) %>% 
-      mutate(across(.cols = all_of(env_cov), .fns = function(x){as.vector(scale(x))})) %>% 
-      ungroup()
+      mutate(across(.cols = c(all_of(guild),all_of(env_cov)), .fns = function(x){as.vector(scale(x))}))
   } else if(metric == "biomass") {
     dataset %>%
-      group_by_at(.vars = cols) %>%
-      summarise(n = sum(biomass), .groups = "drop") %>% 
-      spread(species, n, fill = 0) %>% 
-      # Add unique rownames that describe the site and transect:
-      mutate(loc = make.unique(paste(site, trans, sep = "_"))) %>% 
-      group_by(loc) %>%
-      column_to_rownames("loc") %>% 
-      filter(!is.na(depth)) %>% 
+      filter(species %in% guild,
+             !is.na(depth)) %>% 
+      pivot_wider(names_from = species, values_from = biomass, values_fill = 0, values_fn = sum) %>% 
       select(all_of(guild), all_of(covariate)) %>% 
-      mutate(across(.cols = c(all_of(guild),all_of(env_cov)), .fns = function(x){as.vector(scale(x))})) %>% 
-      ungroup()
+      mutate(across(.cols = c(all_of(guild),all_of(env_cov)), .fns = function(x){as.vector(scale(x))}))
   } else {
     stop("Metric should be either 'sp.n', 'abundance' or 'biomass'")
   }
