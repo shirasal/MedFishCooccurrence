6372183cdd1ff717eca81f5156fc1bd935f81448
Shira Salingré <shira.salin@gmail.com>
2022-09-15 20:03:11 +0200

    updated the data wrangling script to include only what's needed

--- R/data_preps.R
+++ R/data_preps.R
@@ -9,7 +9,8 @@ str(medata)
 
 # Create a dataset with relevant information, scaled covariates, and boolean MPA column:
 med_clean <- medata %>%
-  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
+  mutate(enforce = as.integer(enforcement)) %>% 
+  mutate(mpa = if_else(enforce <= 1, FALSE, TRUE),
          temp = tmean,
          depth = depth,
          prod = pp_mean,
@@ -20,39 +21,17 @@ med_clean <- medata %>%
 medata %>% distinct(country)
 med_clean_east <- medata %>%
   filter(country == "Israel" | country == "Greece" | country == "Turkey") %>% 
-  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
+  mutate(enforce = as.integer(enforcement)) %>% 
+  mutate(mpa = if_else(enforce <= 1, FALSE, TRUE),
          temp = tmean,
          depth = depth,
          prod = pp_mean,
          biomass = (a*sp.length^b)*sp.n) %>%
     select(site, lon, lat, trans, species, sp.n, biomass, mpa, temp, depth, prod)
 
-# write_rds(med_clean, "data/processed/med_clean.rds")
-# write_rds(med_clean_east, "data/processed/med_clean_east.rds")
-
-
-## Abundance matrices ------------------------------------------------------
-
-grps_mat <- create_spp_mat(dataset = med_clean, guild = groupers, metric = "sp.n", covariate = all_covs)
-dip_mat <- create_spp_mat(dataset = med_clean, guild = diplodus, metric = "sp.n", covariate = all_covs)
-herb_mat <- create_spp_mat(dataset = med_clean_east, guild = herbivores, metric = "sp.n", covariate = all_covs)
-
-# Export species matrices
-write_rds(grps_mat, "data/processed/grps_mat.rds")
-write_rds(dip_mat, "data/processed/dip_mat.rds")
-write_rds(herb_mat, "data/processed/herb_mat.rds")
-
-## Biomass matrices ------------------------------------------------------
-
-grps_mass_mat <- create_spp_mat(dataset = med_clean, guild = groupers, metric = "biomass", covariate = all_covs)
-dip_mass_mat <- create_spp_mat(dataset = med_clean, guild = diplodus, metric = "biomass", covariate = all_covs)
-herb_mass_mat <- create_spp_mat(dataset = med_clean_east, guild = herbivores, metric = "biomass", covariate = all_covs)
-
-
-# Export species matrices
-write_rds(grps_mass_mat, "data/processed/grps_mass_mat.rds")
-write_rds(dip_mass_mat, "data/processed/dip_mass_mat.rds")
-write_rds(herb_mass_mat, "data/processed/herb_mass_mat.rds")
+# Export medata
+write_rds(med_clean, "data/processed/med_clean.rds")
+write_rds(med_clean_east, "data/processed/med_clean_east.rds")
 
 # Create dataset relevant for my analysis ---------------------------------
 # Long format, for checks etc.
