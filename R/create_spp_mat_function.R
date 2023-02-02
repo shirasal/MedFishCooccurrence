b5d40b08f5a19a6774df4367031e53232ed90070
Shira Salingre <salingre@mail.tau.ac.il>
2023-02-02 16:39:30 +0100

    updated species matrice creation function to avoid getting Inf values when log-transforming

--- R/create_spp_mat_function.R
+++ R/create_spp_mat_function.R
@@ -13,15 +13,14 @@ create_spp_mat <- function(dataset, guild, metric, covariate){
       # keep only the relevant covariates
       select(all_of(guild), all_of(covariate)) %>% 
       # standard scale environmental covariates
-      mutate(across(.cols = c(all_of(env_cov)), .fns = function(x){as.vector(scale(log10(x)))}))
+      mutate(across(.cols = c(all_of(env_cov)), .fns = function(x){as.vector(scale(log10(x+1)))}))
   } else if(metric == "biomass") {
     dataset %>%
       # mutate(biomass = a*10^b) %>% # modify this in case the dataset doesn't contain biomass
       filter(species %in% guild) %>% 
       pivot_wider(names_from = species, values_from = biomass, values_fn = sum, values_fill = 0) %>% 
       select(all_of(guild), all_of(covariate)) %>% 
-      mutate(across(.cols = c(all_of(guild), all_of(env_cov)), .fns = function(x){log10(x)})) %>% 
-      mutate(across(all_of(guild), .fns = function(x){if_else(is.finite(x), x, 0)})) %>% 
+      mutate(across(.cols = c(all_of(guild), all_of(env_cov)), .fns = function(x){log10(x+1)})) %>% 
       mutate(across(.cols = c(all_of(guild), all_of(env_cov)), .fns = function(x){as.vector(scale(x))}))
   } else {
     stop("Metric should be either 'sp.n', 'abundance' or 'biomass'")
