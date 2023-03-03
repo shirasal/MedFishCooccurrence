784a68e9629cb79734fdec08c04fb9aa1a7c4d4b
Shira Salingre <salingre@mail.tau.ac.il>
2023-03-03 14:23:27 +0100

    exported gt table of sites metadata

--- R/data_summary.R
+++ R/data_summary.R
@@ -1,4 +1,5 @@
 source("R/packages.R")
+library(gt)
 source("R/functions.R")
 source("R/assist_vectors.R")
 
@@ -10,5 +11,28 @@ data_sum <- my_data %>%
   select(site, lon, lat, unique_trans_id, mpa, temp, depth, prod) %>% 
   distinct()
 
-data_sum %>% write_csv("data/processed/sites_table.csv")
+data_sum %>% 
+  count(site, mpa, temp, depth) %>% 
+  gt() %>% 
+  tab_header(
+    title = "Sites summary",
+    subtitle = "Only transects with at least one the focal species of either group (groupers, seabreams or herbivores) are included."
+  ) %>% 
+  fmt_number(
+    columns = c(temp, depth), 
+    ) %>% 
+  fmt_integer(
+    columns = n
+  ) %>% 
+  cols_align(align = "left") %>% 
+  cols_label(site = md("**Site**"), 
+             mpa = md("**MPA**"), 
+             temp = md("**Temperature (SST)**"), 
+             depth = md("**Depth**"), 
+             n = md("**n Transects**")) %>% 
+  opt_align_table_header(align = "center") %>% gtsave(filename = "sites_table.rtf", path = "data/processed/")
+
+# data_sum %>% 
+#   count(site, mpa, temp, depth) %>% 
+#   write_csv("data/processed/sites_table.csv")
 
